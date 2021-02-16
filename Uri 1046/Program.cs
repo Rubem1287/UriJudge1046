3adaae989aade13395d9f57a4a19e49e76fd4efb
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 13:56:50 -0300

    Add files via upload

---
+++ Uri 1046/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uri_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(vetor[0]);
+            int horaFinal = int.Parse(vetor[1]);
+            int duracao;
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O jogo durou "+duracao+" hora(s)");
+        }
+    }
+}
